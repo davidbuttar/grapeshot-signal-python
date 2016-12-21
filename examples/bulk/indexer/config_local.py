d023c60a4ad7b618cc16c705369cb62cd90132e6
Paul Rudin <paul.rudin@grapeshot.com>
2016-12-21 13:26:11 +0000

    moved bulk processing code to new repo

--- examples/bulk/indexer/config_local.py
+++
@@ -1,2 +0,0 @@
-api_host = "openapi-qa.grapeshot.co.uk"
-api_key = "gs_XB5VRbiXQe2d5GimFX846AY-SdXF0gqnFwBAPQYpl9axzYVJYsHAmo"
--- examples/bulk/kibana-nested/src/core_plugins/kibana/server/routes/api/ingest/index.js
+++
@@ -1,9 +0,0 @@
-import { registerProcessors } from './register_processors';
-import { registerSimulate } from './register_simulate';
-import { registerFieldCapabilities } from './register_field_capabilities';
-
-export default function (server) {
-  registerProcessors(server);
-  registerSimulate(server);
-  registerFieldCapabilities(server);
-}
